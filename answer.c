870d1563d64bb648679ddc133b0826018d097d83
Alexis DUBOIS <aduboi8@teaching.CS.McGill.CA>
2019-12-01 10:04:09 -0500

    final version, everything works

--- answer.c
+++ answer.c
@@ -37,13 +37,13 @@ void main() {
 
 	if(strcmp(command,"NORTH")==0) {
 		//send user to the next room
-		printf("<a href=\"http://cs.mcgill.ca/~bromer2/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go NORTH</a>",goldPc);
+		printf("<a href=\"http://cs.mcgill.ca/~bcolli17/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go NORTH</a>",goldPc);
 	} else if(strcmp(command,"SOUTH")==0) {
 		//send user to next room
 		printf("<a href=\"http://cs.mcgill.ca/~zyu29/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go SOUTH</a>",goldPc);
 	} else if(strcmp(command,"EAST")==0) {
 		//send user to next room
-		printf("<a href=\"http://cs.mcgill.ca/~atan16/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go EAST</a>",goldPc);
+		printf("<a href=\"http://cs.mcgill.ca/~ywang498/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go EAST</a>",goldPc);
 	} else if(strcmp(command,"WEST")==0) {
 		//send user to next room
 		printf("<a href=\"http://cs.mcgill.ca/~yradev/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go WEST</a>",goldPc);
@@ -58,20 +58,23 @@ void main() {
 		//user input the right answer
 		goldPc += 10;
 		printf("Correct! You have %d gold pieces. <br /><a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back. </a>", goldPc, goldPc);	
+		//check if the amount of gold is either 100 or 0 and send the user to the appropriate webpage
+		if(goldPc >= 100) {
+			//send user to page that displays "YOU WIN!"
+			printf("You have won! <br /> <a href=\"http://cs.mcgill.ca/~aduboi8\">Press HERE to start a new game</a>");
+	
+		}
+
 	} else {
 		//if the user's input doesn't match any of the possibilities, reduce the amount of gold by 5
 		goldPc -= 5;
 		printf("Wrong input. You have %d gold pieces left. <br /> <a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back.</a>", goldPc, goldPc);
-	}
-
-	//check if the amount of gold is either 100 or 0 and send the user to the appropriate webpage
-	if(goldPc >= 100) {
-		//send user to page that displays "YOU WIN!"
-		printf("You have won! <br /> <a href=\"http://cs.mcgill.ca/~aduboi8\">Press HERE to start a new game</a>");
-	}
-	if(goldPc <= 0) {
-		//send user to a page that displays "YOU LOSE!"
-		printf("You have lost! <br /> <a href=\"https://www.cs.mcgill.ca/~aduboi8\">Press HERE to start over</a>");
+	
+	
+		if(goldPc <= 0) {
+			//send user to a page that displays "YOU LOSE!"
+			printf("You have lost! <br /> <a href=\"https://www.cs.mcgill.ca/~aduboi8\">Press HERE to start over</a>");
+		}
 	}
 	
 	printf("</body>");
