2b2e7e2e4bf0e2f8c9fd32484dccd804e5a5d525
Alexis DUBOIS <aduboi8@teaching.CS.McGill.CA>
2019-12-01 09:21:43 -0500

    adapted version, testing remains

--- addgold.c
+++ addgold.c
@@ -10,37 +10,32 @@ void main() {
 	
 	//capture the gold element in the payload
 	char *data = getenv("QUERY_STRING");
-	char *tokens = strtok(data, "=&?");
-	char input[10];
+	strtok(data, "=");
+	
 	//payload looks like: [URL]/addgold?gold=integer
-	int i=0;
-	while(tokens != NULL) {
-		sscanf(tokens,"%s", &input[i]);
-		tokens = strtok(NULL,"=&?");
-		i++;
-	}
-	//the integer for gold should be in the index 2
-	int gold = atoi(&input[2]);
+	
+	int gold = atoi(strtok(NULL,""));
+	
+	printf("Content-Type:text/html\n\n");
+	printf("<html>");
+	printf("<head> </head>");
+	printf("<body>");
 
 
 	//open the index.html file of the target webpage and print to the screen 
-	FILE *p = fopen("index.html","rt");
+	FILE *p = fopen("../index.html","rt");
+	if(p==NULL) printf("Error trying to open the index.html file");
 	char line[1000];
-	fgets(line,999,p);
-	
-	char *flag = "<form";
-
-	while(!feof(p)) {
-		//search for the form tag in each line, if we find it we insert our new tag right after
-		char *ptr = strstr(line, flag);
-		if( ptr != NULL) {
-			printf("%s",line);
-			printf("<input type=\"hidden\" name=\"gold\" value=\"%d\"",gold);
+	while(fgets(line,999,p)){
+		char tag[20];
+		sscanf(line, "%s", tag);
+		if(strcmp(tag, "<form")==0){
+			printf("<form action=\"answer.cgi\" method=\"get\">\n");
+			printf("<input type=\"hidden\" name=\"gold\" value=\"%d\">",gold);
 		} else {
-			printf("%s",line);
-		}
-		//read the next line into the buffer
-		fgets(line,999,p);
+			printf("%s", line);
+		}	
+
 	}
 
 	fclose(p);
--- answer.c
+++ answer.c
@@ -9,82 +9,76 @@ void main() {
 	
 	//payload loosk like : /cgi-bin/answer?input=answer
 	char *data = getenv("QUERY_STRING");
-	char north[]="NORTH",south[]="SOUTH",east[]="EAST",west[]="WEST",gold[]="GOLD",answer[]="LINK";
-	
 
 	//create a token seperated array to extract the payload data
-	char *tokens = strtok(data,"=&?");
-	char input[10];
-	
-	//paste the payload into an array
-	int i=0;
-	while(tokens != NULL) {
-		input[i] = *tokens;	
-		tokens = strtok(NULL,"=&?");
-		i++;
-	}
-	//assuming the payload looks like: [URL]/answer?input=Command&gold=50
-	//the first token contains the url and program name - so everything before the '?'
-	//the second token contains the name of the CGI variable we ask the user to fill-out
-	//the third token contains the user's input
-	//the fourth and fifth tokens are potentially optional, we must check to make sure
-	//the fourth field is the CGI hidden variable
-	//the fifth field is the amount of gold - don't assume it's an int
-	
-	char *command = &input[2];
-	
+	char *firstTok = strtok(data,"=&");
+
+	//assuming the payload looks like: "[URL]/answer?input=Command&gold=50"
+	//the first token contains the name of the CGI variable we ask the user to fill-out
+	//the second token contains the user's input
+	//the third and fourth tokens are potentially optional, we must check to make sure
+	//the third field is the CGI hidden variable
+	//the fourth field is the amount of gold - don't assume it's an int
 	int goldPc;
-	//check if an amount of gold was provided, if not we will assume 10
-	if(&input[4] == NULL) {
-		goldPc = 10;
+	char *command;
+	if(strcmp(firstTok,"input")==0){
+		command = strtok(NULL,"=&");
+		if(strtok(NULL,"&=")==NULL) goldPc =10;
+		else goldPc =atoi(strtok(NULL,""));
 	} else {
-		sscanf(&input[4],"%d", &goldPc);
+		goldPc = atoi(strtok(NULL,"&="));
+		if(strtok(NULL,"=&")==NULL) command = strtok(NULL,"");
 	}
+
 
 	//compare the command passed with potential options
 	//NORTH SOUTH EAST WEST GOLD LINK
+	
+	printf("Content-Type:text/html\n\n");
+	printf("<html>");
+	printf("<body>");
 
-	if(strcmp(command,north)==0) {
+	if(strcmp(command,"NORTH")==0) {
 		//send user to the next room
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~bromer2/cgi-bin/addgold?gold=%d\">Press HERE to go NORTH</a></html>",goldPc);
-	} else if(strcmp(command,south)==0) {
+		printf("<a href=\"http://cs.mcgill.ca/~bromer2/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go NORTH</a>",goldPc);
+	} else if(strcmp(command,"SOUTH")==0) {
 		//send user to next room
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~zyu29/cgi-bin/addgold?gold=%d\">Press HERE to go SOUTH</a></html>",goldPc);
-	} else if(strcmp(command,east)==0) {
+		printf("<a href=\"http://cs.mcgill.ca/~zyu29/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go SOUTH</a>",goldPc);
+	} else if(strcmp(command,"EAST")==0) {
 		//send user to next room
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~atan16/cgi-bin/addgold?gold=%d\">Press HERE to go EAST</a></html>",goldPc);
-	} else if(strcmp(command,west)==0) {
+		printf("<a href=\"http://cs.mcgill.ca/~atan16/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go EAST</a>",goldPc);
+	} else if(strcmp(command,"WEST")==0) {
 		//send user to next room
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~yradev/cgi-bin/addgold?gold=%d\">Press HERE to go WEST</a></html>",goldPc);
-	} else if(strcmp(command,gold)==0) {
+		printf("<a href=\"http://cs.mcgill.ca/~yradev/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go WEST</a>",goldPc);
+	} else if(strcmp(command,"GOLD")==0) {
 		//send tu user to an other page where the gold number is shown
 		//also include a hyper link to come back to the initial page
 
-		printf("Content-Type:text/html\n\n");
-		printf("<html>");
-		printf("<body><h5>You currently have %d gold pieces.<\\h5><\\body>",goldPc);
-		printf("</html>");
+		 printf("You currently have %d gold pieces.<br /> <a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back.</a>",goldPc,goldPc);
+		
 
-
-	} else if(strcmp(tokens,answer)==0) {
+	} else if(strcmp(command,"LINK")==0 || strcmp(command,"Link")==0) {
 		//user input the right answer
-		goldPc += 10;	
+		goldPc += 10;
+		printf("Correct! You have %d gold pieces. <br /><a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back. </a>", goldPc, goldPc);	
 	} else {
 		//if the user's input doesn't match any of the possibilities, reduce the amount of gold by 5
 		goldPc -= 5;
+		printf("Wrong input. You have %d gold pieces left. <br /> <a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back.</a>", goldPc, goldPc);
 	}
 
 	//check if the amount of gold is either 100 or 0 and send the user to the appropriate webpage
-	if(goldPc == 100) {
+	if(goldPc >= 100) {
 		//send user to page that displays "YOU WIN!"
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~aduboi8/win.html\"></html>");
+		printf("You have won! <br /> <a href=\"http://cs.mcgill.ca/~aduboi8\">Press HERE to start a new game</a>");
 	}
 	if(goldPc <= 0) {
 		//send user to a page that displays "YOU LOSE!"
-		printf("<html><a href=\"https://www.cs.mcgill.ca/~aduboi8/lose.html\"></html>");
+		printf("You have lost! <br /> <a href=\"https://www.cs.mcgill.ca/~aduboi8\">Press HERE to start over</a>");
 	}
-
-
+	
+	printf("</body>");
+	printf("</html>");
 }
 
 
